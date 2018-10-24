263210cbba63584f2111b366dec8b5bf09e0b787
Ali <atajadod94@gmail.com>
2018-10-24 14:22:21 +0200

    subj analysis

--- scripts/analysis/subject_analysis.m
+++ scripts/analysis/subject_analysis.m
@@ -8,10 +8,27 @@ for participant = all_participants
      myparticipant.set_trials();
 end
 
+
+pnum = 1;
 for participant = all_participants
-     myparticipant = participant{:};
+       myparticipant = participant{:};
+       fname = char(strcat('participant_', string(pnum), '.mat'));
+       save(char(fname) , 'myparticipant');
+       pnum = pnum + 1;
+end
+
+
+clear;
+for pnum = 4:15
+     fname = char(strcat('participant_', string(pnum), '.mat'));
+     myparticipant = load(fname);
+     myparticipant = myparticipant.myparticipant;
      for trial_no = 1:myparticipant.NUM_TRIALS
         thistrial = myparticipant.TRIALS{trial_no};
         thistrial.fixationmap()
      end
+     fname = strcat('map',fname);
+     save(char(fname) , 'myparticipant','-v7.3');
+     myparticipant = [];
+     clearvars -except pnum
 end

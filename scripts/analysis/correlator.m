daf262530b7a0fa0ef46fe66f3cb4783076fbb3e
Ali <atajadod94@gmail.com>
2018-11-01 14:12:58 +0100

    Correlator update

---
+++ scripts/analysis/correlator.m
@@ -0,0 +1,5 @@
+resized_subj_data = imresize(subj_data,size(stim_data));
+
+corr(resized_subj_data,stim_data);
+
+
--- scripts/analysis/data_agumentation.m
+++ scripts/analysis/data_agumentation.m
@@ -1,9 +1,8 @@
-for participant_num  = 5
+for participant_num  = [1:4,6:15]
     dirfolder = '/Users/Ali/Desktop/Baycrest/saliency_analysis/Data/Participants/';
     fname = strcat('mapparticipant_',num2str(participant_num));
     participant = load([dirfolder,fname]);
     participant = participant.myparticipant;
-    raw_data = participant.RAW;
     analysis_data = cell(participant.NUM_TRIALS,4);
     
     for trial_no = 1:participant.NUM_TRIALS
@@ -11,7 +10,7 @@ for participant_num  = 5
         try
             trial = trial{:};
         catch
-            continue
+            a = 2;
         end
         analysis_data{trial_no,1} = trial.fixations.image_name{1,1};
         analysis_data{trial_no,2} = trial.fixations.map;
@@ -19,17 +18,22 @@ for participant_num  = 5
         imgidx = get_index(cellstr(trial.fixations.image_name{1,1}), ...
         trial.fixations.raw{1,6},img_name); 
         heat_map = imgSMs.imgMaps(:,:,imgidx);
-        analysis_data{trial_no,3} =  heat_map;
+        analysis_data{trial_no,4} =  heat_map;
     end  
     save(['participant_',num2str(participant_num)],'analysis_data','-v7.3')
 
 end
 
 function hidx = get_index(im_name , number, img_name)
-  name = img_name{:};
+  name = im_name{:};
   name = lower(name);
   number = num2str(number);
   myimage = [number, '_' ,name ];
   hidx = find(strcmp([img_name(:)], myimage));
-  
+  if isempty(hidx)
+        hidx = find(strcmp([img_name(:)], ['0',myimage]));
+  end
+  if isempty(hidx)
+        hidx = find(strcmp([img_name(:)], ['00',myimage]));
+  end
 end
